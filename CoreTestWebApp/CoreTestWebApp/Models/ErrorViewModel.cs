f569d7fca49ccd447b0bf854446bcdc8c595561a
Matteo Tomasulo <matteo.tomasulo@teorema.net>
2020-06-19 12:13:02 +0200

    primo commit

---
+++ CoreTestWebApp/CoreTestWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreTestWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
