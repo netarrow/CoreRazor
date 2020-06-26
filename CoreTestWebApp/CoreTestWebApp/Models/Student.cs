29d50a898b3f4557f8deaa0aaf08c61cf8252aa2
Matteo Tomasulo <matteo.tomasulo@teorema.net>
2020-06-26 12:08:11 +0200

    inserita label e text per nome e cognome

--- CoreTestWebApp/CoreTestWebApp/Models/Student.cs
+++ CoreTestWebApp/CoreTestWebApp/Models/Student.cs
@@ -1,9 +1,13 @@
-﻿namespace CoreTestWebApp.Models
+﻿using System.ComponentModel;
+
+namespace CoreTestWebApp.Models
 {
     public class Student
     {
+        [DisplayName("Nome")]
         public string Name { get; set; }
 
+        [DisplayName("Cognome")]
         public string LastName { get; set; }    
     }
 }
