7913ff0a87ce17e3d3fa7279f29e1bdf87c6b3dd
Matteo Tomasulo <matteo.tomasulo@teorema.net>
2020-06-26 11:57:58 +0200

    creato controller student, creata form di aggiunta studente. testato passaggio dati

---
+++ CoreTestWebApp/CoreTestWebApp/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreTestWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreTestWebApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult AddForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent(Student student)
+        {
+            var tmp = student;
+
+            return View("AddForm");
+        }
+
+    }
+}
