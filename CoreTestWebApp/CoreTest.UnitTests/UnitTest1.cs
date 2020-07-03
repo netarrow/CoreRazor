1f22eb57645acc609f2eaf6456c9946e65804607
Matteo Tomasulo <matteo.tomasulo@teorema.net>
2020-07-03 10:10:59 +0200

    aggiunto unit tests di prova

---
+++ CoreTestWebApp/CoreTest.UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace CoreTest.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Fail();
+        }
+    }
+}
